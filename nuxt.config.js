fee1123230433c326f9b32293e428e9fdc14dc48
okfoxy <hbfxy.17bl@gmail.com>
2020-03-02 21:25:04 +0900

    mod css framework for vuetify

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,67 +1,72 @@
-import pkg from './package'
+import pkg from "./package";
 
 export default {
-    mode: 'spa',
+  mode: "spa",
 
-    /*
-    ** Headers of the page
-    */
-    head: {
-        title: pkg.name,
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: pkg.description }
-        ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-        ]
-    },
-
-    /*
-    ** Customize the progress-bar color
-    */
-    loading: { color: '#fff' },
-
-    /*
-    ** Global CSS
-    */
-    css: [
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: pkg.description }
     ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
 
-    /*
-    ** Plugins to load before mounting the App
-    */
-    plugins: [
-    ],
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
 
-    /*
-    ** Nuxt.js modules
-    */
-    modules: [
-        // Doc:https://github.com/nuxt-community/modules/tree/master/packages/bulma
-        '@nuxtjs/bulma',
-        '@nuxtjs/axios',
-    ],
+  /*
+   ** Global CSS
+   */
+  css: [],
 
-    axios: {
-    },
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
 
-    /*
-    ** Build configuration
-    */
-    build: {
-        postcss: {
-            preset: {
-                features: {
-                    customProperties: false
-                }
-            }
-        },
-        /*
-        ** You can extend webpack config here
-        */
-        extend(config, ctx) {
-        }
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc:https://github.com/nuxt-community/modules/tree/master/packages/bulma
+    // '@nuxtjs/bulma',
+    "@nuxtjs/vuetify",
+    "@nuxtjs/axios"
+  ],
+
+  vuetify: {
+    // Vuetify の設定はここに書く
+    theme: {
+      primary: "#3f51b5",
+      secondary: "#b0bec5",
+      accent: "#8c9eff",
+      error: "#b71c1c"
     }
-}
+  },
+
+  axios: {},
+
+  /*
+   ** Build configuration
+   */
+  build: {
+    postcss: {
+      preset: {
+        features: {
+          customProperties: false
+        }
+      }
+    },
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  }
+};
